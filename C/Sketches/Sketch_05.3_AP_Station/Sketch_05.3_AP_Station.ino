494aa79c7aba9c3768eae76872727b00ddcce3c4
Zhentao-Lin <62634190+Zhentao-Lin@users.noreply.github.com>
2023-03-03 16:33:51 +0800

    Update Sketch_05.3_AP_Station.ino

--- C/Sketches/Sketch_05.3_AP_Station/Sketch_05.3_AP_Station.ino
+++ C/Sketches/Sketch_05.3_AP_Station/Sketch_05.3_AP_Station.ino
@@ -6,8 +6,8 @@
 **********************************************************************/
 #include <WiFi.h>
  
-const char *ssid_Router     =  "FYI_2.4G";  //Enter the router name
-const char *password_Router =  "freenove208";  //Enter the router password
+const char *ssid_Router     =  "********";  //Enter the router name
+const char *password_Router =  "********";  //Enter the router password
 const char *ssid_AP         =  "WiFi_Name"; //Enter the router name
 const char *password_AP     =  "12345678";  //Enter the router password
 
