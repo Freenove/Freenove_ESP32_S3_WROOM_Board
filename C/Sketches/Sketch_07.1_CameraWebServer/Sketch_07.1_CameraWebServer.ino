5331f0ca7fad91615f5fc840a73d0f72b1452bcd
Zhentao-Lin <173039613@qq.com>
2025-01-08 16:59:41 +0800

    Update Sketch_07.1_CameraWebServer.ino

--- C/Sketches/Sketch_07.1_CameraWebServer/Sketch_07.1_CameraWebServer.ino
+++ C/Sketches/Sketch_07.1_CameraWebServer/Sketch_07.1_CameraWebServer.ino
@@ -60,12 +60,12 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_UXGA;
+  config.frame_size = FRAMESIZE_SVGA;
   config.pixel_format = PIXFORMAT_JPEG; // for streaming
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
   config.jpeg_quality = 12;
-  config.fb_count = 1;
+  config.fb_count = 2;
   
   // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
   // for larger pre-allocated frame buffer.
@@ -75,7 +75,7 @@ void setup() {
     config.grab_mode = CAMERA_GRAB_LATEST;
   } else {
     // Limit the frame size when PSRAM is not available
-    config.frame_size = FRAMESIZE_SVGA;
+    config.fb_count = 1;
     config.fb_location = CAMERA_FB_IN_DRAM;
   }
 
