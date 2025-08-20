afb1d80c199ffc46daa573ea1cd0f64c463eef50
Eeason Shen <109536807+Vegetable-SYC@users.noreply.github.com>
2025-08-20 11:05:40 +0800

    update

--- Python/Python_Firmware/mac.py
+++ Python/Python_Firmware/mac.py
@@ -4,4 +4,4 @@ import time
 
 os.system("python3 esptool/esptool.py --chip esp32s3  erase_flash")
 
-os.system("python3 esptool/esptool.py --chip esp32s3 --baud 2000000 write_flash -z 0 GENERIC_S3-20220618-v1.19.1.bin")
+os.system("python3 esptool/esptool.py --chip esp32s3 --baud 2000000 write_flash -z 0 ESP32_GENERIC_S3-SPIRAM_OCT-20250809-v1.26.0.bin")
